a105d92555efce6f8b9dbc58d48411a78e750127
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-10-29 00:01:50 -0400

    ListTile Instead of Custom Card

--- section_4_widgets/lib/widgets/transaction_list.dart
+++ section_4_widgets/lib/widgets/transaction_list.dart
@@ -34,44 +34,25 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Theme.of(context).primaryColorDark,
-                                width: 2)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColorDark,
-                          ),
-                        ),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                            child: Text("\$${transactions[index].amount}")),
                       ),
-                      Container(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(transactions[index].title,
-                                style: Theme.of(context).textTheme.titleMedium),
-                            Text(
-                              DateFormat.yMMMd()
-                                  .format(transactions[index].date),
-                              style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontStyle: FontStyle.italic,
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
+                    ),
+                    title: Text(transactions[index].title,
+                        style: Theme.of(context).textTheme.titleMedium),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
                   ),
                 );
               },
