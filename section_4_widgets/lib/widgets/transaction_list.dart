34725a52827e5b7a42885c7d3744aa87682877ae
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-10-29 17:03:04 -0400

    Section 4 - Widgets: Personal Expenses App Complete

--- section_4_widgets/lib/widgets/transaction_list.dart
+++ section_4_widgets/lib/widgets/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 597,
+      height: 650,
       child: transactions.isEmpty
           ? Column(
               children: [
@@ -25,11 +25,12 @@ class TransactionList extends StatelessWidget {
                   height: 50,
                 ),
                 Container(
-                    height: 400,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    ))
+                  height: 400,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
               ],
             )
           : ListView.builder(
