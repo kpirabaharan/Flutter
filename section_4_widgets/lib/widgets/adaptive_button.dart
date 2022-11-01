b676cbf20e9c58ee60847be0eb3b2c9d562c26c0
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-10-31 23:57:08 -0400

    User Created Adaptive Widgets
    Custom Made TextButton and TextField

---
+++ section_4_widgets/lib/widgets/adaptive_button.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdaptiveTextButton extends StatelessWidget {
+  final String text;
+  final Function handler;
+
+  AdaptiveTextButton(this.text, this.handler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            onPressed: () => handler,
+            child: Text(
+              text,
+            ),
+          )
+        : TextButton(
+            onPressed: () => handler,
+            child: Text(
+              text,
+            ),
+          );
+  }
+}
