d99b0cefa54ded60f41f96d07b8ce39c7682cab1
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-20 22:17:08 -0500

    Section 7: Created a Categories Class With Dummy Data
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ section_7_meals_app/lib/categories_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: [
+        
+      ],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
