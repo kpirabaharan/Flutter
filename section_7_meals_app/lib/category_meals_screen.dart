c650391d506e0b3ffd0787c89295a55ea7d64b21
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-22 20:21:20 -0500

    Section 7: Select and Display Meals By Category
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section_7_meals_app/lib/category_meals_screen.dart
+++ section_7_meals_app/lib/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './models/category.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -9,13 +10,18 @@ class CategoryMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs = ModalRoute.of(context)!.settings.arguments as Category;
 
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(routeArgs.id);
+    }).toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(routeArgs.title),
-      ),
-      body: Center(
-        child: Text('The Recipes For The Category!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(routeArgs.title),
+        ),
+        body: ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Text(categoryMeals[index].title);
+            },
+            itemCount: categoryMeals.length));
   }
 }
