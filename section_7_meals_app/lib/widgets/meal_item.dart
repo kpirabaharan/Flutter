b6b3a67e57b314a00da4ab7eea5f689fed89dccd
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-22 23:33:45 -0500

    Section 7: Added Elegant Title for Meal Item Picture
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section_7_meals_app/lib/widgets/meal_item.dart
+++ section_7_meals_app/lib/widgets/meal_item.dart
@@ -33,6 +33,21 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 250,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      item.title,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
             )
