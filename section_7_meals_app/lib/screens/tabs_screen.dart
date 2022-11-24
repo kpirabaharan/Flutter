c63b204b543c7455edcb053bb12a3e259cb4e5f1
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-23 23:16:57 -0500

    Section 7: Changed TabBar to a BottomNavigationBar
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section_7_meals_app/lib/screens/tabs_screen.dart
+++ section_7_meals_app/lib/screens/tabs_screen.dart
@@ -10,27 +10,51 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, dynamic>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Favorites',
+    }
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-          appBar: AppBar(
-            title: Text('Meals App'),
-            bottom: TabBar(tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              )
-            ]),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.shifting,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            icon: Icon(Icons.category),
+            label: 'Categories',
           ),
-          body: TabBarView(
-            children: [CategoriesScreen(), FavoritesScreen()],
-          )),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
     );
   }
 }
