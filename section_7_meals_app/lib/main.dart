8643bcaa43287ef8575d5d21f75c90a6fd928a17
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-23 20:34:26 -0500

    Section 7: Added Fallback Screen Using onUnknownRoute
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section_7_meals_app/lib/main.dart
+++ section_7_meals_app/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'DeliMeals',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blueGrey)
-            .copyWith(secondary: Colors.redAccent),
+            .copyWith(secondary: Colors.orangeAccent),
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
@@ -30,6 +30,13 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      // onGenerateRoute: (settings) {
+      //   print(settings.arguments);
+      //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
       },
     );
   }
