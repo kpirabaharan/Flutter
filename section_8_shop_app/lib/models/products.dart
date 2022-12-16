b82298136d013dcfa20cc5b628d09b35ef9ea95c
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-12-15 22:30:55 -0500

    Section 8: Filter Data on Popup Button Selection
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section_8_shop_app/lib/models/products.dart
+++ section_8_shop_app/lib/models/products.dart
@@ -38,12 +38,27 @@ class Products with ChangeNotifier {
     ),
   ];
 
+  var _showFavoritesOnly = false;
+
   List<Product> get items {
+    if (_showFavoritesOnly) {
+      return _items.where((prodItem) => prodItem.isFavorite).toList();
+    }
     return [..._items];
   }
 
   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
+  }
+
+  void showFavoritesOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
   }
 
   void addProduct() {
--- section_8_shop_app/lib/screens/products_overview_screen.dart
+++ section_8_shop_app/lib/screens/products_overview_screen.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
+import '../models/products.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
         actions: [
           PopupMenuButton(
-            onSelected: (int selectedValue) {
-              print(selectedValue);
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorites) {
+                productsContainer.showFavoritesOnly();
+              } else {
+                productsContainer.showAll();
+              }
             },
             itemBuilder: (_) => [
-              PopupMenuItem(child: Text('Only Favorites'), value: 0),
-              PopupMenuItem(child: Text('Show All'), value: 1),
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
             ],
             icon: Icon(Icons.more_vert),
           )
