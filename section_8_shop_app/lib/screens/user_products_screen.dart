1418a7a95b3f07d5fc200ff08d92bb094be67c80
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-01 17:32:01 -0500

    Section 10: Added Pull to Refresh on Products Screen
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section_8_shop_app/lib/screens/user_products_screen.dart
+++ section_8_shop_app/lib/screens/user_products_screen.dart
@@ -9,6 +9,10 @@ import '../widgets/user_product_item.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -27,15 +31,18 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: productsData.items.length,
-          itemBuilder: (_, index) => Column(
-            children: [
-              UserProductItem(productsData.items[index]),
-              Divider(),
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: productsData.items.length,
+            itemBuilder: (_, index) => Column(
+              children: [
+                UserProductItem(productsData.items[index]),
+                Divider(),
+              ],
+            ),
           ),
         ),
       ),
