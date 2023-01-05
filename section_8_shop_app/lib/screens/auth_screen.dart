41e8b7e755e45aeafd68905df6be42351699a8b0
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-05 18:25:48 -0500

    Section 12: Controlled Animation Using AnimatiomBuilder
    Not listener. Leads to less re-building of static child widgets.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section_8_shop_app/lib/screens/auth_screen.dart
+++ section_8_shop_app/lib/screens/auth_screen.dart
@@ -120,7 +120,7 @@ class _AuthCardState extends State<AuthCard>
       begin: Size(double.infinity, 260),
       end: Size(double.infinity, 320),
     ).animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
-    _heightAnimation.addListener(() => setState(() {}));
+    // _heightAnimation.addListener(() => setState(() {}));
     super.initState();
   }
 
@@ -212,11 +212,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: const EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+            height: _heightAnimation.value.height,
+            constraints:
+                BoxConstraints(minHeight: _heightAnimation.value.height),
+            width: deviceSize.width * 0.75,
+            padding: const EdgeInsets.all(16.0),
+            child: ch),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
