cde2bc4c7988504f97913ade94b9b5a2dc775ef9
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-05 19:37:43 -0500

    Section 12: Added Order Screen Animations
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section_8_shop_app/lib/widgets/order_screen_item.dart
+++ section_8_shop_app/lib/widgets/order_screen_item.dart
@@ -18,28 +18,36 @@ class _OrderScreenItemState extends State<OrderScreenItem> {
   var _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget.orderItem.amount}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.orderItem.dateTime),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded
+          ? min(widget.orderItem.products.length * 20.0 + 115, 200)
+          : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget.orderItem.amount.toStringAsFixed(2)}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm')
+                    .format(widget.orderItem.dateTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
               padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.orderItem.products.length * 20.0 + 20, 160),
+              height: _expanded
+                  ? min(widget.orderItem.products.length * 20.0 + 20, 160)
+                  : 0,
               child: ListView(
                 children: widget.orderItem.products
                     .map(
@@ -61,7 +69,8 @@ class _OrderScreenItemState extends State<OrderScreenItem> {
                     .toList(),
               ),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
