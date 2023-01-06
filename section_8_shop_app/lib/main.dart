4e0ee56c02cb7a39e5bdf4865afd55584a2c0488
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-05 20:59:47 -0500

    Section 12 Complete: Implemented Custom Route Transitions
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section_8_shop_app/lib/main.dart
+++ section_8_shop_app/lib/main.dart
@@ -13,6 +13,7 @@ import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
 import './screens/splash_screen.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -48,17 +49,23 @@ class MyApp extends StatelessWidget {
         builder: (ctx, auth, _) => MaterialApp(
           title: 'MyShop',
           theme: ThemeData(
-            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
-                .copyWith(secondary: Colors.deepOrange),
-            fontFamily: 'Lato',
-            textTheme: TextTheme(
-              bodyText1: TextStyle(color: Colors.white),
-              headline6: TextStyle(
-                  color: Colors.black,
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
+              colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                  .copyWith(secondary: Colors.deepOrange),
+              fontFamily: 'Lato',
+              textTheme: TextTheme(
+                bodyText1: TextStyle(color: Colors.white),
+                headline6: TextStyle(
+                    color: Colors.black,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold),
+              ),
+              // Make default screen transition theme
+              pageTransitionsTheme: PageTransitionsTheme(
+                builders: {
+                  TargetPlatform.android: CustomPageTransitionBuilder(),
+                  TargetPlatform.iOS: CustomPageTransitionBuilder(),
+                },
+              )),
           home: auth.isAuth
               ? ProductsOverviewScreen()
               : FutureBuilder(
--- section_8_shop_app/lib/widgets/app_drawer.dart
+++ section_8_shop_app/lib/widgets/app_drawer.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../providers/auth.dart';
 import '../screens/orders_screen.dart';
 import '../screens/user_products_screen.dart';
+// import '../helpers/custom_route.dart';
 
 class AppDrawer extends StatelessWidget {
   Widget buildListTile(
@@ -44,6 +45,9 @@ class AppDrawer extends StatelessWidget {
             () {
               Navigator.of(context)
                   .pushReplacementNamed(OrdersScreen.routeName);
+              // Change just this screen
+              // Navigator.of(context).pushReplacement(
+              //     CustomRoute(builder: (ctx) => OrdersScreen()));
             },
           ),
           Divider(),
