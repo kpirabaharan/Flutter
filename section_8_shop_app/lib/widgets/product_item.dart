9c98af1da6dd8026b5b5b443e8e64e99651e9e96
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-05 19:07:25 -0500

    Section 12: Added Hero Animation for Products
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section_8_shop_app/lib/widgets/product_item.dart
+++ section_8_shop_app/lib/widgets/product_item.dart
@@ -40,10 +40,13 @@ class ProductItem extends StatelessWidget {
               arguments: product.id,
             );
           },
-          child: FadeInImage(
-            placeholder: AssetImage('assets/images/product-placeholder.png'),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         footer: GridTileBar(
